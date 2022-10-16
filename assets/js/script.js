de344fdbfc6796957284c0d6d9c67e3b74827ae4
tolapao <62795957+XANuN@users.noreply.github.com>
2022-10-17 00:14:03 +0700

    added modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,17 @@
 'use strict';
 
 
+// modal variables
+const modal = document.querySelector('[data-modal]');
+const modalCloseBtn = document.querySelector('[data-modal-close]');
+const modalCloseOverlay = document.querySelector('[data-modal-overlay]');
+
+// modal function
+const modalCloseFunc = function () { modal.classList.add('closed') }
+
+// modal eventListener
+modalCloseOverlay.addEventListener('click', modalCloseFunc);
+modalCloseBtn.addEventListener('click', modalCloseFunc);
 
 const navbar = document.querySelector("[data-navbar]");
 const navbarLinks = document.querySelectorAll("[data-nav-link]");
