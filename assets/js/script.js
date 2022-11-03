73d2c4a1ad30eca74331f58b0a1bd0ee3ff85be9
tolapao <62795957+XANuN@users.noreply.github.com>
2022-11-04 01:33:35 +0700

    changed
    Co-Authored-By: made by tl <73347353+madebytl@users.noreply.github.com>

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,8 +9,16 @@ toastCloseBtn.addEventListener('click', function () {
   notificationToast.classList.add('closed');
 });
 
+// this script below i found by myself
+
 let x = Math.floor((Math.random() * 4) + 1);
 document.getElementById("number").innerHTML = x;
+
+let y = Math.floor((Math.random() * 2) + 1);
+document.getElementById("d").innerHTML = y + '$';
+
+let z = Math.floor((Math.random() * 4) + 1);
+document.getElementById("d2").innerHTML = z + '$';
 
 
 // modal variables
