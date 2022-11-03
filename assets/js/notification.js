726ce144f57c2763e89c41c21a87f4efb89e7d3c
tolapao <62795957+XANuN@users.noreply.github.com>
2022-11-04 00:49:41 +0700

    changed
    Co-Authored-By: made by tl <73347353+madebytl@users.noreply.github.com>

---
+++ assets/js/notification.js
@@ -0,0 +1,9 @@
+
+// notification toast variables
+const notificationToast = document.querySelector('[data-toast]');
+const toastCloseBtn = document.querySelector('[data-toast-close]');
+
+// notification toast eventListener
+toastCloseBtn.addEventListener('click', function () {
+    notificationToast.classList.add('closed');
+});
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,14 @@
 'use strict';
 
+// notification toast variables
+const notificationToast = document.querySelector('[data-toast]');
+const toastCloseBtn = document.querySelector('[data-toast-close]');
+
+// notification toast eventListener
+toastCloseBtn.addEventListener('click', function () {
+  notificationToast.classList.add('closed');
+});
+
 
 // modal variables
 const modal = document.querySelector('[data-modal]');
