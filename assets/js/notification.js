9e02a1ee6c0e6580d52d7b41881778d4fe19e9e4
tolapao <62795957+XANuN@users.noreply.github.com>
2022-11-04 01:40:01 +0700

    changed
    Co-Authored-By: made by tl <73347353+madebytl@users.noreply.github.com>

--- assets/js/notification.js
+++ assets/js/notification.js
@@ -8,4 +8,15 @@ toastCloseBtn.addEventListener('click', function () {
     notificationToast.classList.add('closed');
 });
 
-document.getElementById("number").innerHTML = Math.random();
+// document.getElementById("number").innerHTML = Math.random();
+
+// this script below i found by myself
+
+let x = Math.floor((Math.random() * 4) + 1);
+document.getElementById("number").innerHTML = x;
+
+let y = Math.floor((Math.random() * 2) + 1);
+document.getElementById("d").innerHTML = y;
+
+let z = Math.floor((Math.random() * 4) + 1);
+document.getElementById("d2").innerHTML = z + '$';
