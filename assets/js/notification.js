8e1f4a088f90bc33553e0805fee44f4bda7312c7
tolapao <62795957+XANuN@users.noreply.github.com>
2022-11-04 20:52:21 +0700

    changed

--- assets/js/notification.js
+++ assets/js/notification.js
@@ -15,7 +15,7 @@ toastCloseBtn.addEventListener('click', function () {
 let x = Math.floor((Math.random() * 4) + 1);
 document.getElementById("number").innerHTML = x;
 
-let y = Math.floor((Math.random() * 2) + 1);
+let y = Math.floor((Math.random() * 4) + 1);
 document.getElementById("d").innerHTML = y;
 
 let z = Math.floor((Math.random() * 4) + 1);
