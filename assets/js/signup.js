ef9e91a3940bd7b22a99085576acda1afbcdd1e6
tolapao <62795957+XANuN@users.noreply.github.com>
2022-12-04 00:26:14 +0700

    Update signup.js

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -1,10 +1,7 @@
 const form = document.querySelector("form"),
     emailField = form.querySelector(".email-field"),
     emailInput = emailField.querySelector(".email"),
-    // passField = form.querySelector(".create-password"),
-    // passInput = passField.querySelector(".password"),
-    // cPassField = form.querySelector(".confirm-password"),
-    // cPassInput = cPassField.querySelector(".cPassword"),
+
 
     claimField = form.querySelector(".claim-field"),
     claimInput = claimField.querySelector(".claim")
@@ -25,8 +22,8 @@ function validateUsername() {
 
 // Email Validtion
 function checkEmail() {
-    const emaiPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
-    if (!emailInput.value.match(emaiPattern)) {
+    const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    if (!emailInput.value.match(emailPattern)) {
         return emailField.classList.add("invalid"); //adding invalid class if email value do not mathced with email pattern
     }
     emailField.classList.remove("invalid"); //removing invalid class if email value matched with emaiPattern
@@ -48,41 +45,40 @@ eyeIcons.forEach((eyeIcon) => {
 });
 
 // Password Validation
-function createPass() {
-    const passPattern = 8;
+// function createPass() {
+//     const passPattern = 8;
 
-    if (!passInput.value.match(passPattern)) {
-        return passField.classList.add("invalid"); //adding invalid class if password input value do not match with passPattern
-    }
-    passField.classList.remove("invalid"); //removing invalid class if password input value matched with passPattern
-}
+//     if (!passInput.value.match(passPattern)) {
+//         return passField.classList.add("invalid"); //adding invalid class if password input value do not match with passPattern
+//     }
+//     passField.classList.remove("invalid"); //removing invalid class if password input value matched with passPattern
+// }
 
 // Confirm Password Validtion
-function confirmPass() {
-    if (passInput.value !== cPassInput.value || cPassInput.value === "") {
-        return cPassField.classList.add("invalid");
-    }
-    cPassField.classList.remove("invalid");
-}
+// function confirmPass() {
+//     if (passInput.value !== cPassInput.value || cPassInput.value === "") {
+//         return cPassField.classList.add("invalid");
+//     }
+//     cPassField.classList.remove("invalid");
+// }
 
 // Calling Funtion on Form Sumbit
 form.addEventListener("submit", (e) => {
     e.preventDefault(); //preventing form submitting
     validateUsername();
     checkEmail();
-    createPass();
-    confirmPass();
+    // createPass();
+    // confirmPass();
 
     //calling function on key up
     emailInput.addEventListener("keyup", validateUsername);
     emailInput.addEventListener("keyup", checkEmail);
-    passInput.addEventListener("keyup", createPass);
-    cPassInput.addEventListener("keyup", confirmPass);
+    // passInput.addEventListener("keyup", createPass);
+    // cPassInput.addEventListener("keyup", confirmPass);
 
     if (
         !emailField.classList.contains("invalid") &&
-        !passField.classList.contains("invalid") &&
-        !cPassField.classList.contains("invalid")
+        !claimField.classList.contains("invalid")
     ) {
         location.href = form.getAttribute("action");
     }
